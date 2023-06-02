3d5356ac5d3bce03296d56ec8c96408cfa96c89b
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-06-02 18:12:44 +0200

    Zaraz bloki

---
+++ src/altanic/content/AltBlocks.java
@@ -0,0 +1,17 @@
+package altanic.content;
+
+import mindustry.content.*;
+import mindustry.type.*;
+import mindustry.world.*;
+
+import multicraft;
+
+public class ALtBlocks
+{
+    public static Block blastFurnace, cokeOven;
+
+    public static void load()
+    {
+        
+    }
+}
