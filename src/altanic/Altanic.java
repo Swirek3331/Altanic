edd907bd7e96ca3285b7954a98553d81580906ae
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-06-02 17:41:14 +0200

    Moje standardy

---
+++ src/altanic/Altanic.java
@@ -0,0 +1,23 @@
+package altanic;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.content.*;
+
+public class Altanic extends Mod
+{
+    public Altanic()
+    {
+        Log.info("Trzyszcz");
+    }
+
+    @Override
+    public void loadContent()
+    {
+        Log.info("Trzyszcz");
+    }
+}
