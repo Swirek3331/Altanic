d231b6d4f4d7005663d27b3717a01c6fd8254302
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-06-04 13:54:03 +0200

    Update AltBlocks.java

--- src/altanic/content/AltBlocks.java
+++ src/altanic/content/AltBlocks.java
@@ -2,12 +2,14 @@ package altanic.content;
 
 import  arc.struct.Seq;
 
-import mindustry.*;
+import mindustry.gen.Sounds;
+import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.type.LiquidStack;
 import mindustry.world.*;
 import static mindustry.content.Items.*;
 import static mindustry.content.Liquids.*;
+import mindustry.world.draw.*;
 
 import multicraft.*;
 
@@ -22,6 +24,12 @@ public class AltBlocks
         blastFurnace = new MultiCrafter("blast-furnace")
         {{
             size = 3;
+            health = 500;
+
+            ambientSound = Sounds.smelter;
+            ambientSoundVolume = 0.04f;
+
+            requirements(Category.crafting, ItemStack.with(lead, 50, metaglass, 25, graphite, 100, titanium, 45, thorium, 75));
 
             resolvedRecipes = Seq.with(
                     new Recipe(
@@ -36,6 +44,20 @@ public class AltBlocks
                             ),
                             180
                     ),
+
+                    new Recipe(
+                            new IOEntry(
+                                    Seq.with(ItemStack.with(feco, 5, coal, 10, flux, 5)),
+                                    Seq.with(),
+                                    2
+                            ),
+                            new IOEntry(
+                                    Seq.with(ItemStack.with(wallAlloy, 5)),
+                                    Seq.with(LiquidStack.with(slag, 0.1f))
+                            ),
+                            180
+                    ),
+
                     new Recipe(
                             new IOEntry(
                                 Seq.with(ItemStack.with(feco, 5, coalCoke, 5)),
@@ -48,8 +70,50 @@ public class AltBlocks
                                     Seq.with(LiquidStack.with(slag, 0.1f))
                             ),
                             180
+                    ),
+
+                    new Recipe(
+                            new IOEntry(
+                                    Seq.with(ItemStack.with(feco, 5, coalCoke, 5, flux, 5)),
+                                    Seq.with(),
+                                    2
+                            ),
+
+                            new IOEntry(
+                                    Seq.with(ItemStack.with(wallAlloy, 5)),
+                                    Seq.with(LiquidStack.with(slag, 0.1f))
+                            ),
+                            180
+                    ),
+
+                    new Recipe(
+                            new IOEntry(
+                                    Seq.with(ItemStack.with(feco, 10, petroleumCoke, 5)),
+                                    Seq.with(),
+                                    6
+                            ),
+                            new IOEntry(
+                                    Seq.with(ItemStack.with(wallAlloy, 10)),
+                                    Seq.with(LiquidStack.with(slag, 0.2f))
+                            ),
+                            180
+                    )
+
+                    new Recipe(
+                            new IOEntry(
+                                    Seq.with(ItemStack.with(feco, 10, petroleumCoke, 5, flux, 5)),
+                                    Seq.with(),
+                                    2
+                            ),
+                            new IOEntry(
+                                    Seq.with(ItemStack.with(wallAlloy, 10)),
+                                    Seq.with(LiquidStack.with(slag, 0.2f))
+                            ),
+                            180
                     )
             );
+
+            drawer = new DrawMulti(new DrawDefault(), new DrawFlame());
         }};
     }
 }
