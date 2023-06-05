486b4013b0f69babd0a7bbab1ec20d8df2a58c60
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-06-05 10:41:54 +0200

    Raz jeszcze

--- src/altanic/content/AltSerpuloTechTree.java
+++ src/altanic/content/AltSerpuloTechTree.java
@@ -21,7 +21,7 @@ public class AltSerpuloTechTree
     {
         //This can be replaced with a lambda expression?
 
-        vanillaNode(Liquids.slag, () -> {//Ciecze mają takie same nazwy jak ich bloki
+        mergeNode(Liquids.slag, () -> {//Ciecze mają takie same nazwy jak ich bloki
             node(feco, () -> {
                 node(wallAlloy, () -> {
                     node(flux);
@@ -29,49 +29,29 @@ public class AltSerpuloTechTree
             });
         });
 
-        vanillaNode(coal, () -> {
+        mergeNode(coal, () -> {
            node(coalCoke, () -> {
                node(sulfur);
                node(petroleumCoke);
            });
         });
 
-        vanillaNode(Liquids.water, () -> {
+        mergeNode(Liquids.water, () -> {
             node(biomass);
             node(wood, () -> {
                 node(creosote);
             });
         });
 
-        vanillaNode(melter, () -> {
+        mergeNode(melter, () -> {
            node(pyrolyseOven, () -> {
                node(blastFurnace);
            });
         });
     }
 
-    //https://github.com/MEEPofFaith/prog-mats-java/blob/erekir/src/progressed/content/PMTechTree.java
-        private static void vanillaNode(UnlockableContent parent, Runnable children)
-        {
-            vanillaNode("serpulo", parent, children);
-        }
-
-        private static void vanillaNode(String tree, UnlockableContent parent, Runnable children)
-        {
-            context = findNode(TechTree.roots.find(r -> r.name.equals(tree)), n -> n.content == parent);
-            children.run();
-        }
-
-        private static TechNode findNode(TechNode root, Boolf<TechNode> filter)
-        {
-            if (filter.get(root))
-                return root;
-            for (TechNode node : root.children)
-            {
-                TechNode search = findNode(node, filter);
-                if (search != null)
-                    return search;
-            }
-            return null;
-        }
+    private static void mergeNode(UnlockableContent parent, Runnable children){
+        context = TechTree.all.find(t -> t.content == parent);
+        children.run();
+    }
 }
