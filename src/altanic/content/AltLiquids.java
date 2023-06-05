abc58cb3770b76d407edafbe86f312ee8ef01e45
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-06-05 18:55:01 +0200

    Ciecze

--- src/altanic/content/AltLiquids.java
+++ src/altanic/content/AltLiquids.java
@@ -2,10 +2,11 @@ package altanic.content;
 
 import arc.graphics.*;
 import mindustry.type.Liquid;
+import mindustry.content.Liquids;
 
 public class AltLiquids
 {
-    public static Liquid creosote;
+    public static Liquid creosote, lightOil, gas, heavyOil;
 
     public static void load()
     {
@@ -17,6 +18,34 @@ public class AltLiquids
             heatCapacity = 0.5f;
             gasColor = color;
             boilPoint = 0.5f;
+        }};
+        
+        lightOil = new Liquid("light-oil", Color.valueOf("ddc600"))
+        {{
+            gasColor = color;
+            viscosity = oil.viscosity * 0.8;
+            flammability = oil.flammability * 2;
+            explosiveness = oil.explosiveness * 0.75f;
+            heatCapacity = oil.heatCapacity;
+            boilPoint = oil.boilPoint;
+        }};
+
+        gas = new Liquid("gas", Color.valueOf("1D1D23"))
+        {{
+            gas = true;
+            gasColor = color;
+            flammability = oil.flammability * 1.5f;
+            explosiveness = oil.explosiveness * 0.8f;
+            heatCapacity = oil.heatCapacity;
+        }};
+
+        heavyOil = new Liquid("heavy-oil", Color.valueOf("3d0000"))
+        {{
+            viscosity = oil.viscosity * 2;
+            flammability = oil.flammability * 0.8f;
+            explosiveness = oil.explosiveness * 1.1f;
+            heatCapacity = oil.heatCapacity;
+            boilPoint = oil.boilPoint;
         }};
     }
 }
