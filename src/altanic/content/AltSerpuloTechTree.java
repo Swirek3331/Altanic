23b38e1442517179b780aae4e501befe9a038d43
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-06-05 10:50:44 +0200

    No to później może

--- src/altanic/content/AltSerpuloTechTree.java
+++ src/altanic/content/AltSerpuloTechTree.java
@@ -1,57 +1,9 @@
 package altanic.content;
 
-import arc.func.*;
-import arc.struct.Seq;
-
-import mindustry.ctype.UnlockableContent;
-import mindustry.content.*;
-import static mindustry.content.TechTree.*;
-import static mindustry.content.Blocks.*;
-import static mindustry.content.Items.*;
-
-import static altanic.content.AltItems.*;
-import static altanic.content.AltBlocks.*;
-import static altanic.content.AltLiquids.*;
-
 public class AltSerpuloTechTree
 {
-    static TechNode context = null;
-
     public static void load()
     {
-        //This can be replaced with a lambda expression?
-
-        mergeNode(Liquids.slag, () -> {//Ciecze mają takie same nazwy jak ich bloki
-            node(feco, () -> {
-                node(wallAlloy, () -> {
-                    node(flux);
-                });
-            });
-        });
-
-        mergeNode(coal, () -> {
-           node(coalCoke, () -> {
-               node(sulfur);
-               node(petroleumCoke);
-           });
-        });
-
-        mergeNode(Liquids.water, () -> {
-            node(biomass);
-            node(wood, () -> {
-                node(creosote);
-            });
-        });
-
-        mergeNode(melter, () -> {
-           node(pyrolyseOven, () -> {
-               node(blastFurnace);
-           });
-        });
-    }
-
-    private static void mergeNode(UnlockableContent parent, Runnable children){
-        context = TechTree.all.find(t -> t.content == parent);
-        children.run();
+        //TODO
     }
 }
