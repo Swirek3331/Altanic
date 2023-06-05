19e82e10e29d51872aeac871a2268da883223b5f
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-06-05 19:16:41 +0200

    Już

--- src/altanic/content/AltLiquids.java
+++ src/altanic/content/AltLiquids.java
@@ -1,8 +1,8 @@
 package altanic.content;
 
-import arc.graphics.*;
+import arc.graphics.Color;
+import mindustry.content.Liquids;
 import mindustry.type.Liquid;
-import mindustry.content.Liquids.*;
 
 public class AltLiquids
 {
@@ -23,29 +23,29 @@ public class AltLiquids
         lightOil = new Liquid("light-oil", Color.valueOf("ddc600"))
         {{
             gasColor = color;
-            viscosity = oil.viscosity * 0.8;
-            flammability = oil.flammability * 2;
-            explosiveness = oil.explosiveness * 0.75f;
-            heatCapacity = oil.heatCapacity;
-            boilPoint = oil.boilPoint;
+            viscosity = (float)(Liquids.oil.viscosity * 0.8);
+            flammability = Liquids.oil.flammability * 2;
+            explosiveness = Liquids.oil.explosiveness * 0.75f;
+            heatCapacity = Liquids.oil.heatCapacity;
+            boilPoint = Liquids.oil.boilPoint;
         }};
 
         gas = new Liquid("gas", Color.valueOf("1D1D23"))
         {{
             gas = true;
             gasColor = color;
-            flammability = oil.flammability * 1.5f;
-            explosiveness = oil.explosiveness * 0.8f;
-            heatCapacity = oil.heatCapacity;
+            flammability = Liquids.oil.flammability * 1.5f;
+            explosiveness = Liquids.oil.explosiveness * 0.8f;
+            heatCapacity = Liquids.oil.heatCapacity;
         }};
 
         heavyOil = new Liquid("heavy-oil", Color.valueOf("3d0000"))
         {{
-            viscosity = oil.viscosity * 2;
-            flammability = oil.flammability * 0.8f;
-            explosiveness = oil.explosiveness * 1.1f;
-            heatCapacity = oil.heatCapacity;
-            boilPoint = oil.boilPoint;
+            viscosity = Liquids.oil.viscosity * 2;
+            flammability = Liquids.oil.flammability * 0.8f;
+            explosiveness = Liquids.oil.explosiveness * 1.1f;
+            heatCapacity = Liquids.oil.heatCapacity;
+            boilPoint = Liquids.oil.boilPoint;
         }};
     }
 }
